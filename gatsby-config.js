1179d68802f4650c04da58c66ea6c426388738bb
woosungchoi <solut2000@gmail.com>
2020-06-18 22:51:26 +0900

    second commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,13 +4,36 @@ require(`dotenv`).config({
 
 module.exports = {
   siteMetadata: {
-    siteTitleAlt: `Emilia - Gatsby Starter Portfolio`,
+    // Used for the title template on pages other than the index site
+    siteTitle: `우성짱`,
+    // Default title of the page
+    siteTitleAlt: `우성짱 - 포트폴리오 블로그`,
+    // Can be used for e.g. JSONLD
+    siteHeadline: `우성짱 - 포트폴리오 블로그`,
+    // Will be used to generate absolute URLs for og:image etc.
+    siteUrl: `https://portfolio.woosung.vercel.app`,
+    // Used for SEO
+    siteDescription: `우성짱의 사진을 모아둔 포트폴리오 블로그입니다. 사진을 공유하며 경험을 나누고 싶습니다.`,
+    // Will be set on the <html /> tag
+    siteLanguage: `ko`,
+    // Used for og:image and must be placed inside the `static` folder
+    siteImage: `/banner.png`,
   },
   plugins: [
     {
       resolve: `@lekoarts/gatsby-theme-emilia`,
       // See the theme's README for all available options
-      options: {},
+      options: {
+		    name: `Woosung`,
+		    location:`South Korea`,
+			formatString:`YYYY.MM.DD`,
+			socialMedia: [
+            {
+              title: `Homepage`,
+              href: `https://www.wsgvet.com`
+			}
+			]
+	  },
     },
     {
       resolve: `gatsby-plugin-google-analytics`,
@@ -22,9 +45,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Emilia - @lekoarts/gatsby-theme-emilia`,
-        short_name: `Emilia`,
-        description: `Minimalistic portfolio/photography site with masonry grid, page transitions and big images. Themeable with Theme UI.`,
+        name: `우성짱의 포트폴리오 블로그`,
+        short_name: `우성짱`,
+        description: `우성짱의 사진을 모아둔 포트폴리오 블로그입니다. 사진을 공유하며 경험을 나누고 싶습니다.`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#3182ce`,
@@ -44,6 +67,5 @@ module.exports = {
       },
     },
     `gatsby-plugin-offline`,
-    `gatsby-plugin-netlify`,
   ],
 }
